4f9ba19d5c35a38ae94984d575dcf5ae2ff68123
Tomasz Olejniczak <snustor@gmail.com>
2019-01-03 14:56:27 +0100

    Project files addition to Task 14.7

--- components/Contact.js
+++ components/Contact.js
@@ -5,14 +5,18 @@ var Contact = React.createClass({
 
     render: function () {
         return (
-            React.createElement('div', {className: 'contactItem'},
-                React.createElement('img', {className: 'contactImage', src: 'http://icons.veryicon.com/256/Application/Long%20Shadow%20Media/Contact.png'}),
-                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-                    this.props.item.email
-                )
-            )
+            <div className = {'contactItem'}>
+                <img className = {'contactImage'} src = {'http://icons.veryicon.com/256/Application/Long%20Shadow%20Media/Contact.png'}/>
+                <p className = {'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className = {'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className = {'contactEmail'} href = {'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
         )
     },
 });
--- components/Contacts.js
+++ components/Contacts.js
@@ -3,13 +3,17 @@ var Contacts = React.createClass({
         items: React.PropTypes.array.isRequired,
     },
 
-    render: function () {
-        var contacts = this.props.items.map(function (contact) {
-            return React.createElement(Contact, {item: contact, key: contact.id});
-        });
-
+    render: function() {
+        var contacts = this.props.items.map(function(contact) {
+            return (
+                <Contact item={contact} key={contact.id}/>
+            )
+      });
+  
         return (
-            React.createElement('ul', {className: 'contactsList'}, contacts)
+            <ul className={'contactsList'}>
+                {contacts}
+            </ul>
         );
     }
 });
